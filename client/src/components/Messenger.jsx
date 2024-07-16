25dc587e02132638831da0954fbefee441bdd1f5
PRABHAKAR SINGH <prabhakarsingh63915@gmail.com>
2024-07-16 10:20:31 +0530

    This is commit file

--- client/src/components/Messenger.jsx
+++ client/src/components/Messenger.jsx
@@ -1,16 +1,22 @@
-
-
 import LoginDialog from "./account/LoginDialog";
+import { AppBar, Toolbar,styled } from "@mui/material";
 
 
-
+const Header=styled(AppBar)`
+background-color:#00bfa5;
+height:220px;
+box-shadow:none
+`
 
 const Messenger = () => {
   return (
     <>
-      HELLO
-      <div>hello2</div>
-      <LoginDialog/>
+      <Header>
+        <Toolbar>
+        
+        </Toolbar>
+      </Header>
+      <LoginDialog />
     </>
   );
 };
