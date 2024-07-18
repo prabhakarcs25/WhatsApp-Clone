8cd1d0ab1b191337496eb471b38028c685cfa2f0
PRABHAKAR SINGH <prabhakarsingh63915@gmail.com>
2024-07-18 14:26:05 +0530

    This is commit file 18-07-2024

---
+++ client/src/components/account/FrontPageMobile.jsx
@@ -0,0 +1,99 @@
+//
+
+import { Typography, Box, styled, TextField,Button } from "@mui/material";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import SearchIcon from "@mui/icons-material/Search";
+import InputAdornment from "@mui/material/InputAdornment";
+const OuterLayer = styled(Box)`
+  text-align: center;
+  padding: 60px 0 0 0;
+  border: 2px solid red;
+`;
+
+const FrontPageMobile = ({ onClick }) => {
+  return (
+    <>
+      <OuterLayer>
+        <Typography
+          sx={{ fontSize: "28px", fontFamily: "inherit", fontWeight: "200" }}
+        >
+          Enter phone number
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "15px",
+            lineHeight: "45px",
+            fontFamily: "inherit",
+            fontWeight: "400",
+            opacity: ".6",
+          }}
+        >
+          {" "}
+          Select a country and enter your WhatsApp phone number.
+        </Typography>
+
+        <FormControl sx={{ m: 1, width: 300, mt: 2 }}>
+          <Select sx={{ height: "50px", borderRadius: "20px" }}>
+            <Box
+              sx={{
+                display: "flex",
+                height: "40px",
+                background: "#DCDCDC",
+                margin: "1px 20px 0px 20px",
+                borderRadius: "10px",
+              }}
+            >
+              <SearchIcon sx={{ height: "45px", ml: "5px" }} />
+              <TextField sx={{ width: "100%" }} />
+            </Box>
+
+            <MenuItem>India +91</MenuItem>
+            <MenuItem>America</MenuItem>
+            <MenuItem>NewZeland +91</MenuItem>
+            <MenuItem>Pakkistan +91</MenuItem>
+            <MenuItem>Bagladesh +91</MenuItem>
+          </Select>
+        </FormControl>
+
+        <Box>
+          <TextField
+            id="outlined-start-adornment"
+            sx={{
+              m: 1,
+              width: "300px",
+              borderRadius: "20px",
+              background: "#DCDCDC",
+            }}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">+91</InputAdornment>
+              ),
+            }}
+          />
+        </Box>
+        <Button 
+            style={{
+              backgroundColor: "#017561",
+              border: "none",
+              borderRadius: "20px",
+              color: "white",
+              height: "35px",
+              width: "80px",
+              margin: "15px 0 0 0",
+              textTransform: "unset",
+            }}
+            href="https://mui.com/"
+          >
+            Next
+          </Button>
+          <Typography sx={{color:'#017561', fontFamily:'inherit', fontWeight:'500', lineHeight:'50px'}}>
+            Link with QR code
+          </Typography>
+      </OuterLayer>
+    </>
+  );
+};
+
+export default FrontPageMobile;
--- client/src/components/account/LoginDialog.jsx
+++ client/src/components/account/LoginDialog.jsx
@@ -1,62 +1,7 @@
-import {
-  Dialog,
-  Box,
-  Typography,
-  List,
-  ListItem,
-  styled,
-  Button,
-} from "@mui/material";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import SettingsIcon from "@mui/icons-material/Settings";
-import { qrCodeImage } from "../../constants/data";
-import { GoogleLogin } from "@react-oauth/google";
-import { jwtDecode } from "jwt-decode";
-import logo1 from "../../constants/logo1.png";
-import Divider from "@mui/material/Divider";
-
-const UpperLayer = styled(Box)`
-  height: 100px;
-  border-radius: 5px;
-  width: 80%;
-  border: 2px solid #dcdcdc;
-  margin-top: 40px;
-  transform: translateX(13%);
-`;
-const UpperLayerimg = styled("img")({
-  height: 80,
-  padding: "13px 0 0 25px",
-});
-
-const Component = styled(Box)`
-  display: flex;
-`;
-const Container = styled(Box)`
-  padding: 32px 0 56px 56px;
-`;
-
-const Title = styled(Typography)`
-  font-size: 28px;
-  color: #525252;
-  font-weight: 300;
-  font-family: inherit;
-  margin-bottom: 35px;
-`;
-const StyledList = styled(List)`
-  & > li {
-    padding: 0;
-    margin-top: 15px;
-    font-size: 18px;
-    line-height: 30px;
-    color: #4a4a4a;
-  }
-`;
-
-const QrCode = styled("img")({
-  height: 270,
-  width: 280,
-  margin: "30px 0 0 150px",
-});
+import { Dialog } from "@mui/material";
+import React, { useState } from "react";
+import FrontPageScanner from "./FrontPageScanner";
+import FrontPageMobile from "./FrontPageMobile";
 
 const DialogStyled = {
   height: "96%",
@@ -70,143 +15,28 @@ const DialogStyled = {
 };
 
 const LoginDialog = () => {
-  const onLoginSuccess = (res) => {
-    const jwt_DecodeResult = jwtDecode(res.credential);
-    console.log(jwt_DecodeResult);
+  const [showComponent2, setShowComponent2] = useState(true);
+
+  const handleComponent2ButtonClick = () => {
+    setShowComponent2(false);
   };
 
-  const onLoginError = (res) => {
-    console.log("Error in page", res);
+  const handleComponent3ButtonClick = () => {
+    setShowComponent2(true);
   };
 
   return (
     <>
-      <Dialog open={true} PaperProps={{ sx: DialogStyled }}>
-        <UpperLayer>
-          <Box style={{ display: "flex" }}>
-            <UpperLayerimg src={logo1} alt="logo1 with upperLayer" />
-            <Box>
-              <Typography
-                style={{
-                  margin: "30px 0 0 10px",
-                  fontFamily: "revert",
-                  fontWeight: "400",
-                }}
-              >
-                Download WhatsApp for Windows
-              </Typography>
-              <Typography
-                style={{
-                  margin: "1px 0 0 10px",
-                  fontFamily: "inherit",
-                  fontWeight: "400",
-                  fontSize: "14px",
-                }}
-              >
-                Get calling, screen sharing and a faster experiance with the new
-                Windows app.
-              </Typography>
-            </Box>
-            <Button
-              style={{
-                backgroundColor: "#017561",
-                border: "none",
-                borderRadius: "20px",
-                color: "white",
-                height: "35px",
-                width: "120px",
-                margin: "35px 0 0 15px",
-                textTransform: "unset",
-              }}
-              href="https://mui.com/"
-            >
-              Get the app
-            </Button>
-          </Box>
-        </UpperLayer>
-        <Component>
-          <Container>
-            <Title> Use WhatsApp on your computer</Title>
-            <StyledList>
-              <ListItem>1. Open WhatApp on your phone.</ListItem>
-              <ListItem>
-                2. Tap
-                <Box
-                  component="span"
-                  style={{ fontWeight: "600", marginLeft: "3px" }}
-                >
-                  Menu
-                </Box>
-                <MoreVertIcon style={{ opacity: 0.6 }} /> on Android , or{" "}
-                <Box
-                  component="span"
-                  style={{ fontWeight: "600", marginLeft: "5px" }}
-                >
-                  Settings
-                </Box>{" "}
-                <SettingsIcon style={{ opacity: "0.6", marginLeft: "5px" }} />
-                on iPhone
-              </ListItem>
+      <Dialog open={true} PaperProps={{ sx: DialogStyled }} sx={{userSelect:'none'}}>
+        {/* <FrontPageScanner /> */}
 
-              <ListItem>
-                3. Tap{" "}
-                <Box
-                  component="span"
-                  style={{
-                    fontWeight: "600",
-                    marginLeft: "5px",
-                    marginRight: "4px",
-                  }}
-                >
-                  Linked devices
-                </Box>{" "}
-                and then
-                <Box
-                  component="span"
-                  style={{
-                    fontWeight: "600",
-                    marginLeft: "5px",
-                    marginRight: "4px",
-                  }}
-                >
-                  Link a device
-                </Box>
-                .
-              </ListItem>
-              <ListItem>
-                4. Point your phone at this screen to capture the QR code.
-              </ListItem>
-            </StyledList>
-          </Container>
-          <Box style={{ position: "relative" }}>
-            <QrCode src={qrCodeImage} alt="Qr Code Img" />
-            <Box
-              style={{
-                position: "absolute",
-                top: "45%",
-                transform: "translateX(59%)",
-              }}
-            >
-              <GoogleLogin onSuccess={onLoginSuccess} onError={onLoginError} />
-            </Box>
-          </Box>
-        </Component>
-        <Box sx={{ padding: "0 40px 0 40px" }}>
-          <Divider variant="middle" sx={{ opacity: 1 }} />
-        </Box>
-        <Box>
-          <Typography
-            sx={{
-              color: "#017561",
-              fontFamily: "inherit",
-              fontWeight: "400",
-              fontSize: "18px",
-              padding: "25px 0 0 60px",
-            }}
-          >
-            Link with phone number
-          </Typography>
-        </Box>
+        {showComponent2 ? (
+        <FrontPageScanner onClick={handleComponent2ButtonClick} />
+      ) : (
+        <FrontPageMobile onClick={handleComponent3ButtonClick} />
+      )}
+
+
       </Dialog>
     </>
   );
